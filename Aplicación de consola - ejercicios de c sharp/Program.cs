abcb80bdfe1805802c97454f0f66de63673a73e0
Freddy <freddy_p@hotmail.cl>
2020-01-30 00:08:40 -0300

    Finalizado

---
+++ Aplicación de consola - ejercicios de c sharp/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Aplicación_de_consola___ejercicios_de_c_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string connectString =
+                 "Data Source=(local);User ID=ab;Password=MyPassword;" +
+                 "Initial Catalog=pricing_test";
+
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(connectString);
+
+                //elimino el user y la pw porque se inicia con la autenticación de windows
+                builder.Remove("User ID");
+                builder.Remove("Password");
+                builder.IntegratedSecurity = true;
+
+                // Connect to SQL
+                Console.Write("Conectando a SQL Server ... ");
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    connection.Open();
+                    Console.WriteLine("conectado");
+                    Console.WriteLine("Ejercicio 1-A.");
+                    //Ejercicio 1-A
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append("USE pricing_test;");
+                    sb.Append("select count(*) from proveedor;");
+                    //sb.Append("INSERT INTO proveedor (rut,nombre,telefono,pagina_web,direccion,id_comuna,id_ciudad) values (195845921,'Barbara', 930799532, 'www.pricinprueba.cl','Cuatro poniente 322',1,1);");
+
+                    string query = sb.ToString();
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        var countRows = command.ExecuteScalar();
+                        Console.WriteLine("La cantidad de proveedores es: " + countRows);
+                    }
+                    //Ejercicio 1-B
+                    Console.WriteLine("Ejercicio 1-B.");
+                    sb.Clear();
+                    sb.Append("USE pricing_test;");
+                    sb.Append("select TOP 1 id_producto from venta_detalle GROUP BY id_producto ORDER BY sum(monto_total_producto) desc");
+                    string id_product = sb.ToString();
+
+                    using (SqlCommand command = new SqlCommand(id_product, connection))
+                    {
+                        var id_pro = command.ExecuteScalar();
+                        sb.Clear();
+                        sb.Append("USE pricing_test;");
+                        sb.Append("select rut_proveedor from producto where id = " + id_pro);
+                        string query_to_rut = sb.ToString();
+
+                        using (SqlCommand command2 = new SqlCommand(query_to_rut, connection))
+                        {
+                            var rut_proveedor = command2.ExecuteScalar();
+                            sb.Clear();
+                            sb.Append("USE pricing_test;");
+                            sb.Append("select nombre from proveedor where rut = " + rut_proveedor);
+                            string query_to_name = sb.ToString();
+
+                            using (SqlCommand command3 = new SqlCommand(query_to_name, connection))
+                            {
+                                var name = command3.ExecuteScalar();
+
+                                Console.WriteLine("El mejor proveedor de acuerdo a las ventas de sus productos es: " + name);
+                            }
+                        }
+                    }
+                    //Ejercicio 1-C
+                    Console.WriteLine("Ejercicio 1-C.");
+                    sb.Clear();
+                    sb.Append("USE pricing_test;");
+                    sb.Append("select TOP 1 id_producto from venta_detalle GROUP BY id_producto ORDER BY sum(cantidad_vendida_producto) desc");
+                    id_product = sb.ToString();
+
+                    using (SqlCommand command = new SqlCommand(id_product, connection))
+                    {
+                        var id_pro = command.ExecuteScalar();
+                        sb.Clear();
+                        sb.Append("USE pricing_test;");
+                        sb.Append("select rut_proveedor from producto where id = " + id_pro);
+                        string query_to_rut = sb.ToString();
+
+                        using (SqlCommand command2 = new SqlCommand(query_to_rut, connection))
+                        {
+                            var rut_proveedor = command2.ExecuteScalar();
+                            sb.Clear();
+                            sb.Append("USE pricing_test;");
+                            sb.Append("select nombre from proveedor where rut = " + rut_proveedor);
+                            string query_to_name = sb.ToString();
+
+                            using (SqlCommand command3 = new SqlCommand(query_to_name, connection))
+                            {
+                                var name = command3.ExecuteScalar();
+
+                                Console.WriteLine("El producto mejor vendido: " + name);
+                            }
+                        }
+                    }
+
+                }
+
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine("Presione una tecla para salir");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Ejercicio ASP.NET MVC/Controllers/proveedorController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Ejercicio_ASP.NET_MVC.Models;
+
+namespace Ejercicio_ASP.NET_MVC.Controllers
+{
+    public class proveedorController : Controller
+    {
+        private pricing_testEntities db = new pricing_testEntities();
+
+        // GET: proveedor
+        public ActionResult GetProveedors()
+        {
+            var proveedor = db.proveedor.Include(p => p.ciudad).Include(p => p.comuna);
+            return View(proveedor.ToList());
+        }
+
+        // GET: proveedor/Create
+        public ActionResult Create()
+        {
+            ViewBag.id_ciudad = new SelectList(db.ciudad, "id", "nombre");
+            ViewBag.id_comuna = new SelectList(db.comuna, "id", "nombre");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "rut,nombre,telefono,pagina_web,direccion,id_comuna,id_ciudad")] proveedor proveedor)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    db.proveedor.Add(proveedor);
+                    db.SaveChanges();
+                }
+                catch
+                {
+
+                }
+
+                return RedirectToAction("GetProveedors");
+            }
+
+            ViewBag.id_ciudad = new SelectList(db.ciudad, "id", "nombre", proveedor.id_ciudad);
+            ViewBag.id_comuna = new SelectList(db.comuna, "id", "nombre", proveedor.id_comuna);
+            return RedirectToAction("GetProveedors");
+        }
+
+        // GET: proveedor/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            proveedor proveedor = db.proveedor.Find(id);
+            if (proveedor == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.id_ciudad = new SelectList(db.ciudad, "id", "nombre", proveedor.id_ciudad);
+            ViewBag.id_comuna = new SelectList(db.comuna, "id", "nombre", proveedor.id_comuna);
+            return View(proveedor);
+        }
+
+        [HttpPost]
+        public ActionResult Edit([Bind(Include = "rut,nombre,telefono,pagina_web,direccion,id_comuna,id_ciudad")] proveedor proveedor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(proveedor).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("GetProveedors");
+            }
+            ViewBag.id_ciudad = new SelectList(db.ciudad, "id", "nombre", proveedor.id_ciudad);
+            ViewBag.id_comuna = new SelectList(db.comuna, "id", "nombre", proveedor.id_comuna);
+            return View(proveedor);
+        }
+
+        // GET: proveedor/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            proveedor proveedor = db.proveedor.Find(id);
+            if (proveedor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(proveedor);
+        }
+
+        // POST: proveedor/Delete/5
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            proveedor proveedor = db.proveedor.Find(id);
+            db.proveedor.Remove(proveedor);
+            db.SaveChanges();
+            return RedirectToAction("GetProveedors");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
